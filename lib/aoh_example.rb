d08a921799bb0c80e9adfca219a14c709d314806
Ruari Day <ruariday@gmail.com>
2019-12-11 11:58:57 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,7 @@ end
 def literal_aoh
   literal_hashes_array = [ 
     {:name => "Don Gately", :occupation => "Live-in Staff"},
-    {:name => "Joelle Van Dyne", :occupation => "Radio Personality"},
+    {:name => "Joelle van Dyne", :occupation => "Radio Personality"},
     {:name => "Pat Monteseian", :occupation => "Staff"},
     {:name => "Kate Gompert", :occupation => "None"},
     {:name => "Bruce Green", :occupation => "Fan of Mildred"}
