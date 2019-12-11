3258cf9f8b4c64addcb404aa378f73ff999b578e
Ruari Day <ruariday@gmail.com>
2019-12-11 12:03:25 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,7 +27,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  new_value = 
+  new_value = "Joelle van Dyne (PGOAT)"
   aoh[1][:name] = new_value
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
